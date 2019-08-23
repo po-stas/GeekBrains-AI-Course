d625b6708861d7dca8691b97c45f1cf3faa25bc1
postas <pologrudov@gmail.com>
2019-08-23 16:43:36 +0200

    Commit for Lesson4

---
+++ Lesson4/topic4/select_by_month.sql
@@ -0,0 +1,3 @@
+-- Из таблицы users необходимо извлечь пользователей, родившихся в августе и мае. Месяцы заданы в виде списка английских названий ('may', 'august')
+
+SELECT * FROM users WHERE birth_month = 'may' OR birth_month = 'august';
