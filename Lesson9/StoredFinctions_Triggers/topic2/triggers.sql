b010ca594633fb9391b8537334f5c71453889ab7
postas <pologrudov@gmail.com>
2019-09-14 14:30:27 +0200

    Added trigger for update either

--- Lesson9/StoredFinctions_Triggers/topic2/triggers.sql
+++ Lesson9/StoredFinctions_Triggers/topic2/triggers.sql
@@ -11,6 +11,14 @@ CREATE TRIGGER check_for_null_columns BEFORE INSERT ON products
 			SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Both fields (name and description) cannot be NULL at the same time.';
 		END IF;
 	END//
+
+CREATE TRIGGER check_for_update_null_columns BEFORE UPDATE ON products
+	FOR EACH ROW BEGIN
+		IF NEW.name IS NULL AND NEW.description IS NULL THEN
+			SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Both fields (name and description) cannot be NULL at the same time.';
+		END IF;
+	END//
+
 DELIMITER ;
 
 INSERT INTO `products` (`id`, `name`, `description`, `price`, `catalog_id`) VALUES (1000, NULL, NULL, 1000, 1);
